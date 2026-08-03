15e49f19409404632e38cadeebfef47c11c41e66
Zoto1 <zotonguyen1810@gmail.com>
2026-08-04 02:18:48 +0700

    Add gitignore for CMake C++ project

--- build/CMakeFiles/HybridFTP_Core.dir/src/common/CommandParser.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/HybridFTP_Core.dir/src/common/CommandParser.cpp.o: \
- /workspaces/Hybrid-FTP/src/common/CommandParser.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/HybridFTP_Core.dir/src/common/TCPControl.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/HybridFTP_Core.dir/src/common/TCPControl.cpp.o: \
- /workspaces/Hybrid-FTP/src/common/TCPControl.cpp \
- /usr/include/stdc-predef.h
